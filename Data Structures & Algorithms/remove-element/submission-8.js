170529c8dde332d661b83f35bbd513a81a79e22e
singhharshit <singh.harshit7@gmail.com>
2026-09-24 05:18:14 +0530

    Add: remove-element - submission-8

---
+++ Data Structures & Algorithms/remove-element/submission-8.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+  removeElement(nums, val) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right];
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    return left;
+}
+}
