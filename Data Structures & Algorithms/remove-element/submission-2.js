1076b29ac6cc3ffeedc4e81402508c4111c5a2b3
singhharshit <singh.harshit7@gmail.com>
2026-09-24 04:41:06 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let k=nums.length
+        for(let i=0;i<nums.length;i++)
+        {
+            if(nums[i]===val){
+                k--
+            }
+        }
+let diff=[]
+for(let i=k;i<nums.length;i++){
+    if(nums[i]!==val)
+    diff.push(nums[i])
+}
+let j=0
+    for(let i=0;i<k;i++)
+{
+    if(nums[i]===val )
+    {  
+        
+       let temp = nums[i];
+       nums[i]=diff[j]
+       diff[j]=temp  
+       j++
+    }
+
+}
+return k
+    }
+}
