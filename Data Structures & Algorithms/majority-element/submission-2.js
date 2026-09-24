c6163d53345f61f631edf944ce6ecdbf527dafed
singhharshit <singh.harshit7@gmail.com>
2026-09-24 05:50:18 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+       
+       let map=new Map()
+       for(let i=0;i<nums.length;i++)
+       {
+        if(!map.has(nums[i]))
+        {
+            map.set(nums[i],1)
+        }
+        else{
+        map.set(nums[i],map.get(nums[i])+1)
+        }
+       }
+
+       for(let i=0;i<nums.length;i++)
+       {
+        if(map.get(nums[i])>nums.length/2)
+        {
+            return nums[i]
+        }
+       }
+    }
+}
